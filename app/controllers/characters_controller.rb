69e237c38f2427417fcc1a07bacd1a81c20fd183
christianloring <christianrloring@gmail.com>
2025-08-07 17:39:06 -0400

    some views to make a character

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,54 @@
+class CharactersController < ApplicationController
+  before_action :set_character, only: %i[show edit update destroy]
+
+  def index
+    @characters = current_user.characters
+  end
+
+  def show; end
+
+  def new
+    @character = current_user.characters.build
+  end
+
+  def create
+    @character = current_user.characters.build(character_params)
+    if @character.save
+      redirect_to @character, notice: "Character created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @character.update(character_params)
+      redirect_to @character, notice: "Character updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @character.destroy
+    redirect_to characters_path, notice: "Character deleted."
+  end
+
+  private
+
+  def set_character
+    @character = current_user.characters.find(params[:id])
+  end
+
+  def character_params
+    params.require(:character).permit(
+      :name, :character_class, :subclass, :species, :level,
+      :strength, :dexterity, :constitution,
+      :intelligence, :wisdom, :charisma,
+      :background, :alignment, :backstory,
+      :current_hp, :max_hp, :experience_points,
+      :profile_picture
+    )
+  end
+end
