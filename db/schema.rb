543761b5c1b9d26e953777bf37e99bf60442bad9
christianloring <christianrloring@gmail.com>
2025-08-08 15:24:06 -0400

    Add Hitpoints/Speed to Character and allow Editing Characters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_08_163731) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_08_185736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -93,6 +93,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_08_163731) do
     t.integer "charisma"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "hitpoints"
+    t.integer "speed", default: 30, null: false
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
