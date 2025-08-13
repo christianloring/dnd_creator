50e9e2d8d230c6910502d0e7dffe2ecb52eb2809
christianloring <christianrloring@gmail.com>
2025-08-12 20:10:05 -0400

    home spec

--- spec/request/home_request_spec.rb
+++ spec/request/home_request_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe "Home", type: :request do
   describe "GET /" do
     context "when user is not signed in" do
-      it "redirects to sign in page" do
+      it "returns http success" do
         get root_path
-        expect(response).to redirect_to(new_session_path)
+        expect(response).to have_http_status(:success)
+      end
+
+      it "renders the index template" do
+        get root_path
+        expect(response).to render_template(:index)
       end
     end
 
@@ -17,27 +22,37 @@ RSpec.describe "Home", type: :request do
         expect(response).to have_http_status(:success)
       end
 
-      it "renders the dashboard template" do
+      it "renders the index template" do
         get root_path
+        expect(response).to render_template(:index)
+      end
+    end
+  end
+
+  describe "GET /dashboard" do
+    context "when user is not signed in" do
+      it "redirects to sign in page" do
+        get dashboard_path
+        expect(response).to redirect_to(new_session_path)
+      end
+    end
+
+    context "when user is signed in" do
+      before { session_sign_in }
+
+      it "returns http success" do
+        get dashboard_path
+        expect(response).to have_http_status(:success)
+      end
+
+      it "renders the dashboard template" do
+        get dashboard_path
         expect(response).to render_template(:dashboard)
       end
 
-      it "assigns user's characters" do
-        user = Current.session.user
-        character = create(:character, user: user)
-
-        get root_path
-
-        expect(assigns(:characters)).to include(character)
-      end
-
-      it "assigns user's campaigns" do
-        user = Current.session.user
-        campaign = create(:campaign, user: user)
-
-        get root_path
-
-        expect(assigns(:campaigns)).to include(campaign)
+      it "assigns user count" do
+        get dashboard_path
+        expect(assigns(:user_count)).to be_present
       end
     end
   end
